c3d7a764277d71e3f1c5f9c7d023cc9603b975ad
Sachin Christopher Deshpande <35118726+sachincdeshpande@users.noreply.github.com>
2018-09-10 11:23:21 -0700

    Update PlayerControls.cs

--- Scripts/PlayerControls.cs
+++ Scripts/PlayerControls.cs
@@ -10,17 +10,17 @@ public class PlayerControls : MonoBehaviour {
     // fields that control the player's ship
     // declared public so unity may initialize correctly when specified in the development kit
     public float thrust_strength;                   // how much the spaceship will go up/down
-	public float speed;                             // how much spaceship will go left/right
+    public float speed;                             // how much spaceship will go left/right
 
-	Rigidbody rb;                                   // RigidBody to give spaceship collision
+    Rigidbody rb;                                   // RigidBody to give spaceship collision
 
     // Used for initialization
     // Default method included when script is utilized
     void Start()
     {
         // initialize spaceship's collision
-		rb = this.GetComponent<Rigidbody>();        
-	}
+  	rb = this.GetComponent<Rigidbody>();        
+    }
 
     // Update is called once per frame
     // Default method included when script is utilized
@@ -31,16 +31,16 @@ public class PlayerControls : MonoBehaviour {
         // if user input is invalid, game remains unaffected, input is ignoted
         if (Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow)) {
             // make spaceship go up
-			rb.AddForce (new Vector3 (0, thrust_strength, 0));
-		}
-	    if(Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow)) {
+	    rb.AddForce (new Vector3 (0, thrust_strength, 0));
+	}
+	   if(Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow)) {
             // make spaceship go left
             rb.AddForce (new Vector3(-speed,0,0));
-		}
-		if (Input.GetKey ("d") || Input.GetKey(KeyCode.RightArrow)) {
+	}
+	if (Input.GetKey ("d") || Input.GetKey(KeyCode.RightArrow)) {
             // make spaceship go right
             rb.AddForce (new Vector3 (speed, 0, 0));
-		}
+	}
         if (Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow))
         {
             // make spaceship go down
